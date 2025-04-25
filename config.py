b583a7ceb6d48afde271ae053ac89a42fface630
Anna Safaryan <Anna.Safaryan-813@yandex.ru>
2025-04-26 02:41:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-config = {
+config_dict = {
     "google_prefix": '/content/drive/MyDrive/Colab Notebooks/Diplom',
     "data_prefix": "data",
     "raw_prefix": "raw",
@@ -9,48 +9,72 @@ config = {
     "tab-s_prefix": "tab_s",
     "late interaction prefix": "late_interaction",
     "re-ranking prefix": "re_ranking",
+
+    "marco_name": 'BeIR/msmarco',
+    "marco_qrels_name": 'BeIR/msmarco-qrels',
+
+    
+    'dpr_model': 'facebook/dpr-question_encoder-multiset-base',
+    'colbert_model': 'colbert-ir/colbertv2.0',
+    "ce_model": "cross-encoder/ms-marco-MiniLM-L-6-v2"
+
 }
 
 # СЫРЫЕ ДАННЫЕ
-config['corpus_data_template'] = (config['google_prefix'] + '/' +
-                             config['data_prefix'] + '/' +
-                             config['raw_prefix'] + '/' +
-                             '{}/corpus_test.parquet' # корпус + колонка_сплит
-                             )
-config['corpus_ids_template'] = (config['google_prefix'] + '/' +
-                             config['data_prefix'] + '/' +
-                             config['raw_prefix'] + '/' +
-                             '{}/corpus_test.json' # корпус + колонка_сплит
-                             )
+config_dict['corpus_data_template'] = (config_dict['google_prefix'] + '/' +
+                                       config_dict['data_prefix'] + '/' +
+                                       config_dict['raw_prefix'] + '/' +
+                             '{}/corpus_test.parquet'  # корпус + колонка_сплит
+                                       )
+config_dict['corpus_ids_template'] = (config_dict['google_prefix'] + '/' +
+                                      config_dict['data_prefix'] + '/' +
+                                      config_dict['raw_prefix'] + '/' +
+                             '{}/corpus_test.json'  # корпус + колонка_сплит
+                                      )
 
-config['queries_data_template'] = (config['google_prefix'] + '/' +
-                             config['data_prefix'] + '/' +
-                             config['raw_prefix'] + '/' +
-                             '{}/queries_test.parquet' # корпус + колонка_сплит
-                             )
-config['corpus_ids_template'] = (config['google_prefix'] + '/' +
-                             config['data_prefix'] + '/' +
-                             config['raw_prefix'] + '/' +
-                             '{}/queries_test.json' # корпус + колонка_сплит
-                             )
+config_dict['queries_data_template'] = (config_dict['google_prefix'] + '/' +
+                                        config_dict['data_prefix'] + '/' +
+                                        config_dict['raw_prefix'] + '/' +
+                             '{}/queries_test.parquet'  # корпус + колонка_сплит
+                                        )
+config_dict['queries_ids_template'] = (config_dict['google_prefix'] + '/' +
+                                       config_dict['data_prefix'] + '/' +
+                                       config_dict['raw_prefix'] + '/' +
+                             '{}/queries_test.json'  # корпус + колонка_сплит
+                                       )
 
-config['qrels_data_template'] = (config['google_prefix'] + '/' +
-                             config['data_prefix'] + '/' +
-                             config['raw_prefix'] + '/' +
-                             '{}/qrels_test.parquet' # корпус + колонка_сплит
-                             )
+config_dict['qrels_data_template'] = (config_dict['google_prefix'] + '/' +
+                                      config_dict['data_prefix'] + '/' +
+                                      config_dict['raw_prefix'] + '/' +
+                             '{}/qrels_test.parquet'  # корпус + колонка_сплит
+                                      )
 
+config_dict['data_template'] = (config_dict['google_prefix'] + '/' +
+                                      config_dict['data_prefix'] + '/' +
+                                      config_dict['raw_prefix'] + '/' +
+                             '{}/data_test.parquet'  # корпус + колонка_сплит
+                                      )
 
 # ВЕКТОРА
-config['corpus_vector_template'] = (config['google_prefix'] + '/' +
-                             config['data_prefix'] + '/' +
-                             config['vectors_prefix'] + '/' +
-                             '{}/{}/corpus_test.npz' # метод + корпус + колонка_сплит
-                             )
-config['queries_vector_template'] = (config['google_prefix'] + '/' +
-                             config['data_prefix'] + '/' +
-                             config['vectors_prefix'] + '/' +
-                             '{}/{}/queries_test.npz' # метод + корпус + колонка_сплит
-                             )
+config_dict['corpus_vector_template'] = (config_dict['google_prefix'] + '/' +
+                                         config_dict['data_prefix'] + '/' +
+                                         config_dict['vectors_prefix'] + '/' +
+                             '{}/{}/corpus_test.npz'  # метод + корпус + колонка_сплит
+                                         )
+config_dict['corpus_mapping_template'] = (config_dict['google_prefix'] + '/' +
+                                         config_dict['data_prefix'] + '/' +
+                                         config_dict['vectors_prefix'] + '/' +
+                             '{}/{}/corpus_test.json'  # метод + корпус + колонка_сплит
+                                         )
 
-# print(config)
+
+config_dict['queries_vector_template'] = (config_dict['google_prefix'] + '/' +
+                                          config_dict['data_prefix'] + '/' +
+                                          config_dict['vectors_prefix'] + '/' +
+                             '{}/{}/queries_test.npz'  # метод + корпус + колонка_сплит
+                                          )
+config_dict['queries_mapping_template'] = (config_dict['google_prefix'] + '/' +
+                                          config_dict['data_prefix'] + '/' +
+                                          config_dict['vectors_prefix'] + '/' +
+                             '{}/{}/queries_test.json'  # метод + корпус + колонка_сплит
+                                          )
