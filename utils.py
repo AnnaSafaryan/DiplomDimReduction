e7016a1c981c140ef16de9e5a13b21d6c8ac11d2
Anna Safaryan <Anna.Safaryan-813@yandex.ru>
2025-04-28 03:14:31 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from os import makedirs
+import numpy as np
 
 
 def create_path(path):
@@ -9,3 +10,19 @@ def create_path(path):
         print(f"{path} created.")
     except FileExistsError:
         print(f"{path} exists.")
+
+
+def normalize(X, axis=None, eps=1e-10):
+    # X = np.asarray(X)
+    if X.ndim == 1:
+        norm = np.linalg.norm(X, ord=2)
+        return X / (norm + eps)
+    elif X.ndim == 2:
+        norm = np.linalg.norm(X, ord=2, axis=1 if axis is not None else 1, keepdims=True)
+        return X / (norm + eps)
+    else:
+        raise ValueError("Input must be a 1D or 2D array.")
+
+
+def cosine_similarity(vec, matrix):
+  return normalize(matrix, axis=1) @ normalize(vec).T
